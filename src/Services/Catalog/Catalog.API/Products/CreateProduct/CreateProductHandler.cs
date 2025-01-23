56c86fbbdeb738ea44277c5d432d8aa95734f415
tflion <tfleao07@gmail.com>
2025-01-22 22:35:38 -0300

    Iniciando Create Product slice e alterando portas de execução

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(
+    string Name,
+    List<string> Category,
+    string Description,
+    string ImageFile,
+    decimal Price
+) : IRequest<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+    {
+        //Adicionar regra de negócio para criar um produto.
+        throw new NotImplementedException();
+    }
+}
+ 

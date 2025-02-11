a73aa792c0b22ac9a78f2ac5725c57f9959db379
tflion <tfleao07@gmail.com>
2025-02-10 21:32:37 -0300

    Iniciando microsserviço para o carrinho

---
+++ src/Services/Basket/Basket.API/Basket/GetBasket/GetBasketHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace Basket.API.Basket.GetBasket;
+
+public record GetBasketQuery(string UserName) : IQuery<GetBasketResult>;
+public record GetBasketResult(ShoppingCart Cart);
+
+public class GetBasketQueryHandler : IQueryHandler<GetBasketQuery, GetBasketResult>
+{
+    public async Task<GetBasketResult> Handle(GetBasketQuery query, CancellationToken cancellationToken)
+    {
+        //var basket = await _repository.GetBasket(request.UserName);
+
+        return new GetBasketResult(new ShoppingCart("Test"));
+    }
+}

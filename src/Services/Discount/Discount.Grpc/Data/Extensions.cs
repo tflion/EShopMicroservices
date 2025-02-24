c4af18aedfa6bcfc027f6fdb5d6e34c8f18c4af9
tflion <tfleao07@gmail.com>
2025-02-24 19:55:46 -0300

    Orquestrando discount.grpc com docker compose

--- src/Services/Discount/Discount.Grpc/Data/Extensions.cs
+++ src/Services/Discount/Discount.Grpc/Data/Extensions.cs
@@ -2,13 +2,12 @@
 
 namespace Discount.Grpc.Data;
 
-public static class Extensions
+public static class Extentions
 {
     public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
     {
         using var scope = app.ApplicationServices.CreateScope();
         using var dbContext = scope.ServiceProvider.GetRequiredService<DiscountContext>();
-
         dbContext.Database.MigrateAsync();
 
         return app;

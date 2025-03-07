fc9e246e75c3f766ae2fab5c525351aa385ea7fe
tflion <tfleao07@gmail.com>
2025-03-07 00:07:09 -0300

    Adicionando EFCore para SQL Server com Entity Configurations e Migration inicial

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -10,6 +10,9 @@ public static class DependencyInjection
     {
         var connectionString = configuration.GetConnectionString("Database");
 
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
+
         return services;
     }
 }

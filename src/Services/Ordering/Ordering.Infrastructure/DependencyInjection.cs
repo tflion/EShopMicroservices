66c5b4d5771fd130fac5c1f539030fb445d912ca
tflion <tfleao07@gmail.com>
2025-03-18 23:49:37 -0300

    Adicionando interceptor para auditoria do SaveChanges do EntityFramework

--- src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -11,7 +11,10 @@ public static class DependencyInjection
         var connectionString = configuration.GetConnectionString("Database");
 
         services.AddDbContext<ApplicationDbContext>(options =>
-            options.UseSqlServer(connectionString));
+        {
+            options.AddInterceptors(new AuditableEntityInterceptor());
+            options.UseSqlServer(connectionString);
+        });
 
         return services;
     }
--- src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Infrastructure/GlobalUsing.cs
@@ -1,4 +1,8 @@
 ﻿global using Ordering.Infrastructure.Data;
+global using Ordering.Infrastructure.Data.Interceptors;
 global using Microsoft.EntityFrameworkCore;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
+global using Ordering.Domain.Abstractions;
+
+

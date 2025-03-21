350403792fed36e7e70cd3d899464ea02462faa4
tflion <tfleao07@gmail.com>
2025-03-21 20:06:24 -0300

    Adicionando CommandHandlers e iniciando QueryHandlers

--- src/Services/Ordering/Ordering.Application/GlobalUsing.cs
+++ src/Services/Ordering/Ordering.Application/GlobalUsing.cs
@@ -4,3 +4,8 @@ global using Ordering.Application.Dtos;
 global using Ordering.Domain.Models;
 global using Ordering.Domain.ValueObjects;
 global using Ordering.Application.Exceptions;
+global using MediatR;
+global using Microsoft.Extensions.Logging;
+global using Ordering.Domain.Events;
+global using Microsoft.EntityFrameworkCore;
+
